34cbb5a04788c2aad39690f206e01faadde31c03
sergyleo@yahoo.com <sergyleo@yahoo.com>
2022-12-04 19:09:39 +1300

    first commit

---
+++ readme.dm
@@ -0,0 +1,15 @@
+#File Sorter
+
+#Description:
+You need to copy the file to the folder with files you want to sort.
+The programme read all the files in the folder and create folders according to their creation date.
+Then it copies all the folders to the appropriate folder and checks if there are any copies.
+If there are some copies it adds "(copy)"  and a number to a copy filename before it copies it to a folder.
+
+sorting_by_dd_mmm_yyyy.py -> sorts to folders like "01 Jan 2022"
+sorting_by_mmm_yyyy.py -> sorts to folders like "Jan 2022"
+
+#Language: Python
+#Packages: os, time, shutil
+#Author: Sergey Germanov
+#Year: 2022
---
+++ sorting_by_mmm_yyyy.py
@@ -0,0 +1,59 @@
+import os
+import time
+import shutil
+
+# go to the correct folder
+# os.chdir('Photos')
+file_number = 1
+folder_number = 1
+# go through the list of files
+for file in os.listdir():
+
+    # create the Date String dd Mon yyyy
+    date = time.strftime('%b %Y', time.gmtime(os.path.getmtime(file)))
+    collection_dir = date
+
+    # if it is a folder do not read
+    if os.path.isdir(file):
+        continue
+
+    # if a folder do not exist then create it with the Date string used as a name
+    if not os.path.exists(collection_dir):
+        os.mkdir(collection_dir)
+        print("New Folder has been created: " + collection_dir)
+        folder_number += 1
+
+    # if a file is not in a folder then copy it
+    if not os.path.exists(collection_dir + '/' + file):
+        shutil.copy2(file, collection_dir)
+        print(str(file_number) + " " + file)
+        file_number += 1
+    else:
+        counter = 0
+
+        old_name = os.path.join(collection_dir, file)
+        # get file name without extension
+        only_name = os.path.splitext(file)[0]
+        only_extension = os.path.splitext(file)[1]
+
+        # read the Date filder and if counts how many similar files are there to add as a counter
+        for f_name in os.listdir(collection_dir):
+            if f_name.startswith(only_name):
+                counter += 1
+
+        # Adding the new name with extension
+        new_base = only_name + '_copy(' + str(counter) + ')' + only_extension
+        # construct full file path
+        new_name = os.path.join(collection_dir, new_base)
+
+        # Renaming the file
+        os.rename(old_name, new_name)
+
+        # copy a file with a new name
+        shutil.copy2(file, collection_dir)
+        print(str(file_number) + " " + file)
+        file_number += 1
+
+print("Sorting Completed!")
+print("Total number of folders created: " + str(folder_number))
+print("Total number of files copied: " + str(file_number))
